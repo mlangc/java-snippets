62e2276083560af2a8e257d1d7d8b4bf3d690ec8
Matthias Langer <m.langer798@gmail.com>
2024-01-28 08:50:56 +0100

    Tweak benchmark

--- src/test/java/at/mlangc/benchmarks/GcdImplsTest.java
+++ src/test/java/at/mlangc/benchmarks/GcdImplsTest.java
@@ -10,20 +10,20 @@ import static at.mlangc.benchmarks.GcdImpls.euclidIterative;
 import static at.mlangc.benchmarks.GcdImpls.euclidRecursive;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestGcdImpls {
+public class GcdImplsTest {
     @Property
     void gcdImplementationsAreConsistent(
             @ForAll @LongRange(min = 0) long a,
             @ForAll @LongRange(min = 0) long b) {
         var gcdEuclidIterative = euclidIterative(a, b);
         var gcdEuclidRecursive = euclidRecursive(a, b);
-        var commonsGcd = ArithmeticUtils.gcd(a, b);
-        var guavaGcd = LongMath.gcd(a, b);
+        var gcdCommons = ArithmeticUtils.gcd(a, b);
+        var gcdGuava = LongMath.gcd(a, b);
 
         assertThat(gcdEuclidIterative)
                 .as("a=%s, b=%s", a, b)
                 .isEqualTo(gcdEuclidRecursive)
-                .isEqualTo(commonsGcd)
-                .isEqualTo(guavaGcd);
+                .isEqualTo(gcdCommons)
+                .isEqualTo(gcdGuava);
     }
 }
