f96a0eb0653e1f85a0ccc27c9292cd681740aae7
Matthias Langer <m.langer798@gmail.com>
2025-04-15 00:02:23 +0100

    Working concurrent queues

---
+++ src/main/java/at/mlangc/art/of/multiprocessor/programming/ch10/ConcurrentUnboundedIntQueue.java
@@ -0,0 +1,51 @@
+package at.mlangc.art.of.multiprocessor.programming.ch10;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentUnboundedIntQueue implements IntQueue {
+    private static class Node {
+        final int x;
+        final AtomicReference<Node> next = new AtomicReference<>();
+
+        Node(int x) {
+            this.x = x;
+        }
+    }
+
+    private final AtomicReference<Node> head = new AtomicReference<>(new Node(0));
+    private final AtomicReference<Node> tail = new AtomicReference<>(head.getPlain());
+
+    @Override
+    public void enq(int x) {
+        var node = new Node(x);
+
+        while (true) {
+            var currentTail = tail.get();
+            var previousNext = currentTail.next.compareAndExchange(null, node);
+
+            if (previousNext != null) {
+                tail.compareAndSet(currentTail, previousNext);
+            } else {
+                tail.compareAndSet(currentTail, node);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int deq() {
+        while (true) {
+            var currentHead = head.get();
+            var currentNext = currentHead.next.get();
+
+            if (currentNext == null) {
+                throw new NoSuchElementException();
+            }
+
+            if (head.compareAndSet(currentHead, currentNext)) {
+                return currentNext.x;
+            }
+        }
+    }
+}
--- src/test/java/at/mlangc/art/of/multiprocessor/programming/ch10/IntQueueTest.java
+++ src/test/java/at/mlangc/art/of/multiprocessor/programming/ch10/IntQueueTest.java
@@ -1,22 +1,24 @@
 package at.mlangc.art.of.multiprocessor.programming.ch10;
 
+import org.apache.commons.lang3.exception.UncheckedInterruptedException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.BitSet;
 import java.util.NoSuchElementException;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class IntQueueTest {
     enum QueueImpl {
-        LOCK_BASED(LockBasedUnboundedIntQueue::new);
+        LOCK_BASED(LockBasedUnboundedIntQueue::new),
+        CONCURRENT(ConcurrentUnboundedIntQueue::new);
 
         final Supplier<IntQueue> ctor;
 
@@ -29,7 +31,7 @@ class IntQueueTest {
     @EnumSource
     void enqueuingAndDequeuingConcurrentlyShouldWork(QueueImpl impl) {
         var intQueue = impl.ctor.get();
-        final var limit = 25_000;
+        final var limit = 100_000;
 
         IntFunction<Runnable> enqueuerWithParity = parity -> () -> {
             for (int x = parity; x < limit; x += 2) {
@@ -72,6 +74,7 @@ class IntQueueTest {
         assertThat(enqJob2).succeedsWithin(1, TimeUnit.SECONDS);
         assertThat(deqJob1).succeedsWithin(1, TimeUnit.SECONDS);
         assertThat(deqJob2).succeedsWithin(1, TimeUnit.SECONDS);
+        assertThat(deqJob1.join().intersects(deqJob2.join())).isFalse();
 
         var dequeued = deqJob1.join();
         dequeued.or(deqJob2.join());
@@ -79,4 +82,79 @@ class IntQueueTest {
         assertThat(dequeued.previousSetBit(Integer.MAX_VALUE)).isEqualTo(limit - 1);
     }
 
+    @ParameterizedTest
+    @EnumSource
+    void enqueuingAndDequeueingConcurrentlyShouldWorkInMixedScenario(QueueImpl impl) {
+        final var paralleism = 4;
+        final var itemsPerPhase = 1000;
+        final var totalItems = itemsPerPhase * paralleism * 10;
+        final var allEnqueued = new CountDownLatch(paralleism);
+        final var intQueue = impl.ctor.get();
+
+        IntFunction<Supplier<BitSet>> newJob = jobId -> () -> {
+            var seen = new BitSet(totalItems);
+            var x = jobId;
+            while (x < totalItems) {
+                for (int i = 0; i < itemsPerPhase && x < totalItems; i++) {
+                    intQueue.enq(x);
+                    x += paralleism;
+                }
+
+                while (true) {
+                    try {
+                        var y = intQueue.deq();
+                        assertThat(seen.get(y)).isFalse();
+                        seen.set(y);
+                    } catch (NoSuchElementException ignore) {
+                        break;
+                    }
+                }
+            }
+
+            allEnqueued.countDown();
+            withUncheckedInterrupts(allEnqueued::await);
+
+            while (true) {
+                try {
+                    var y = intQueue.deq();
+                    assertThat(seen.get(y)).isFalse();
+                    seen.set(y);
+                } catch (NoSuchElementException ignore) {
+                    break;
+                }
+            }
+
+            return seen;
+        };
+
+        var jobs = IntStream.range(0, paralleism)
+                .mapToObj(newJob)
+                .map(CompletableFuture::supplyAsync)
+                .toList();
+
+        assertThat(jobs).allSatisfy(job ->
+                assertThat(job).succeedsWithin(1, TimeUnit.SECONDS));
+
+        var seen = new BitSet(totalItems);
+        for (CompletableFuture<BitSet> job : jobs) {
+            var seenFromJob = job.join();
+            assertThat(seen.intersects(seenFromJob)).isFalse();
+            seen.or(seenFromJob);
+        }
+
+        assertThat(seen.cardinality()).isEqualTo(totalItems);
+        assertThat(seen.previousSetBit(Integer.MAX_VALUE)).isEqualTo(totalItems - 1);
+    }
+
+    interface InterruptibleRunnable {
+        void run() throws InterruptedException;
+    }
+
+    private static void withUncheckedInterrupts(InterruptibleRunnable op) {
+        try {
+            op.run();
+        } catch (InterruptedException e) {
+            throw new UncheckedInterruptedException(e);
+        }
+    }
 }
