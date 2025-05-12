44ae6c5f6871bfcca520ef31dd1982b928984df6
Matthias Langer <m.langer798@gmail.com>
2025-05-12 21:59:05 +0200

    Experiment with Peterson Lock

--- src/main/java/at/mlangc/art/of/multiprocessor/programming/ch2/PetersonLock.java
+++ src/main/java/at/mlangc/art/of/multiprocessor/programming/ch2/PetersonLock.java
@@ -2,32 +2,64 @@ package at.mlangc.art.of.multiprocessor.programming.ch2;
 
 import com.google.common.base.Preconditions;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 class PetersonLock {
-    private volatile long victim = -1;
+    private enum MemoryOrdering {
+        SEQ_CST, ACQ_REL;
+    }
+
+    private static final MemoryOrdering MEMORY_ORDERING = MemoryOrdering.SEQ_CST;
+
+    private final AtomicInteger victim = new AtomicInteger(-1);
     private final AtomicIntegerArray locked = new AtomicIntegerArray(2);
 
     void lock() {
         var id = threadId();
 
-        locked.set(id, 1);
-        victim = id;
+        setLocked(id, true);
+        setVictim(id);
 
-        while (locked.get(1 - id) == 1 && victim == id) {
+        while (getLocked(1 - id) && getVictim() == id) {
             Thread.onSpinWait();
         }
     }
 
     void unlock() {
         int id = threadId();
-        Preconditions.checkState(locked.get(id) == 1);
-        locked.set(id, 0);
+        Preconditions.checkState(getLocked(id));
+        setLocked(id, false);
     }
 
     private static int threadId() {
         int id = Integer.parseInt(Thread.currentThread().getName().split(":")[1]);
         Preconditions.checkState(0 <= id && id < 2);
         return id;
+    }
+
+    private boolean getLocked(int i) {
+        return (MEMORY_ORDERING == MemoryOrdering.SEQ_CST ? locked.get(i) : locked.getAcquire(i)) == 1;
+    }
+
+    private void setLocked(int index, boolean value) {
+        if (MEMORY_ORDERING == MemoryOrdering.SEQ_CST) {
+            locked.set(index, value ? 1 : 0);
+        } else {
+            locked.setRelease(index, value ? 1 : 0);
+
+        }
+    }
+
+    private int getVictim() {
+        return MEMORY_ORDERING == MemoryOrdering.SEQ_CST ? victim.get() : victim.getAcquire();
+    }
+
+    private void setVictim(int id) {
+        if (MEMORY_ORDERING == MemoryOrdering.SEQ_CST) {
+            victim.set(id);
+        } else {
+            victim.setRelease(id);
+        }
     }
 }
