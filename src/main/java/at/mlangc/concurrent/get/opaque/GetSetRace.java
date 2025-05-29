2f5329a567f12ae954f534685e655848fe5e5aa6
Matthias Langer <m.langer798@gmail.com>
2025-05-29 17:42:04 +0200

    Working on code for blog post

--- src/main/java/at/mlangc/concurrent/get/opaque/GetSetRace.java
+++ src/main/java/at/mlangc/concurrent/get/opaque/GetSetRace.java
@@ -1,12 +1,13 @@
 package at.mlangc.concurrent.get.opaque;
 
 import at.mlangc.concurrent.MemoryOrdering;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.exception.UncheckedInterruptedException;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 import static java.lang.System.out;
@@ -21,19 +22,24 @@ class GetSetRace {
 
         var rng = ThreadLocalRandom.current();
         var state = new AtomicIntegerArray(128 * 1024 * 1024);
+        var barrier = new CyclicBarrier(2);
         for (int i = 0; i < iterations; i++) {
             var xIdx = rng.nextInt(state.length());
-            int yIdx = rng.ints(0, state.length()).filter(y -> y != xIdx).findAny().orElseThrow();
+            int yIdx = getYIdx(rng, state.length(), xIdx);
             state.setPlain(xIdx, 0);
             state.setPlain(yIdx, 0);
 
             var job1 = CompletableFuture.supplyAsync(() -> {
+                withUncheckedExceptions(barrier::await);
+
                 var r1 = memoryOrdering.get(state, yIdx);
                 memoryOrdering.set(state, xIdx, r1);
                 return r1;
             });
 
             var job2 = CompletableFuture.supplyAsync(() -> {
+                withUncheckedExceptions(barrier::await);
+
                 var r2 = memoryOrdering.get(state, xIdx);
                 memoryOrdering.set(state, yIdx, 42);
                 return r2;
@@ -45,4 +51,25 @@ class GetSetRace {
         out.printf("Results: %s%n", results);
     }
 
+    private static int getYIdx(Random random, int xIdx, int len) {
+        while (true) {
+            var yIdx = random.nextInt(len);
+            if (yIdx != xIdx) return yIdx;
+        }
+    }
+
+    interface RunnableWithCheckedExceptions {
+        void run() throws Exception;
+    }
+
+    static void withUncheckedExceptions(RunnableWithCheckedExceptions op) {
+        try {
+            op.run();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new UncheckedInterruptedException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
