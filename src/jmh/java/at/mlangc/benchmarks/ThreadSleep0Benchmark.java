73db6a220132b19aea0b958c5736378d622a6b98
Matthias Langer <matthias.langer@dynatrace.com>
2025-07-15 22:34:55 +0200

    Working on code for blog-post

--- src/jmh/java/at/mlangc/benchmarks/ThreadSleep0Benchmark.java
+++ src/jmh/java/at/mlangc/benchmarks/ThreadSleep0Benchmark.java
@@ -1,15 +1,19 @@
 package at.mlangc.benchmarks;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 @Fork(value = 1)
-@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 30, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.Throughput)
 public class ThreadSleep0Benchmark {
     @State(Scope.Thread)
@@ -48,9 +52,58 @@ public class ThreadSleep0Benchmark {
         final ThreadLocalRandom rng = ThreadLocalRandom.current();
     }
 
+
+    @State(Scope.Benchmark)
+    public static class BackgroundCpuConsumer {
+        @Param({"false"})
+        private boolean backgroundCpu;
+
+        private volatile boolean stop;
+        private final List<CompletableFuture<Void>> backgroundCpuConsumers = new ArrayList<>();
+
+        @Setup
+        public void setup() {
+            if (!backgroundCpu) return;
+
+            for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+                backgroundCpuConsumers.add(CompletableFuture.runAsync(() -> {
+                    while (!stop) {
+                        Blackhole.consumeCPU(1000);
+                    }
+                }));
+            }
+        }
+
+        @TearDown
+        public void tearDown() {
+            stop = true;
+
+            for (var cpuConsumers : backgroundCpuConsumers) {
+                cpuConsumers.orTimeout(1, TimeUnit.SECONDS).join();
+            }
+        }
+    }
+
+    @State(Scope.Benchmark)
+    public static class CpuTokensConfig {
+        @Param({"85"})
+        private int cpuTokens;
+    }
+
     @Benchmark
-    public void sleep0() throws InterruptedException {
+    public void sleep0(BackgroundCpuConsumer ignore) throws InterruptedException {
         Thread.sleep(0);
+    }
+
+    @Benchmark
+    public void tokensOnly(BackgroundCpuConsumer ignore, CpuTokensConfig tokens) {
+        Blackhole.consumeCPU(tokens.cpuTokens);
+    }
+
+    @Benchmark
+    public void sleep0AndConsumeCpuTokens(CpuTokensConfig tokens) throws InterruptedException {
+        Thread.sleep(0);
+        Blackhole.consumeCPU(tokens.cpuTokens);
     }
 
     @Benchmark
