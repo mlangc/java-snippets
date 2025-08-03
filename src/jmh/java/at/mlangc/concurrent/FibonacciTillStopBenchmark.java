9bcef416291bf95fc44f028502a5498f28238f50
Matthias Langer <m.langer798@gmail.com>
2025-08-03 07:56:59 +0200

    Working on code for blog post

--- src/jmh/java/at/mlangc/concurrent/FibonacciTillStopBenchmark.java
+++ src/jmh/java/at/mlangc/concurrent/FibonacciTillStopBenchmark.java
@@ -10,43 +10,32 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @Fork(value = 1)
 @Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 10, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.Throughput)
 @State(Scope.Benchmark)
 public class FibonacciTillStopBenchmark {
     private final AtomicBoolean stop = new AtomicBoolean();
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     @Param
     private MemoryOrdering memoryOrdering;
-    private Future<?> schedule;
-
-    @Setup
-    public void setup() {
-        schedule = scheduler.scheduleWithFixedDelay(() ->
-                memoryOrdering.set(stop, true), 0, 50, TimeUnit.MILLISECONDS);
-    }
-
-    @TearDown
-    public void tearDown() {
-        schedule.cancel(false);
-        scheduler.shutdown();
-    }
 
     @Benchmark
     public int fibTillStop() {
+        return fibTillStop(memoryOrdering, stop);
+    }
+
+    private static int fibTillStop(MemoryOrdering memoryOrdering, AtomicBoolean stop) {
         final var mod = 1_000_000_007;
         var fib0 = 0;
         var fib1 = 1;
-        var i = 0;
         stop.setPlain(false);
 
-        while (!memoryOrdering.get(stop) && i < 1000) {
+        for (int i = 0; !memoryOrdering.get(stop) && i < 100_000; i++) {
             var fib2 = fib0 + fib1;
             if (fib2 >= mod) fib2 -= mod;
             fib0 = fib1;
             fib1 = fib2;
-            i++;
+
         }
 
         return fib1;
